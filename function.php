61b4868e1bc2e5ceccc54014ed26689f352de65a
Rashik Ishrak Nahian <rashik@nahian.me>
2017-09-22 12:14:03 +0600

    Create function.php

---
+++ function.php
@@ -0,0 +1,6 @@
+add_filter( 'pre_get_posts', 'my_get_posts' );
+ function my_get_posts( $query ) {
+ if ( is_home() && false == $query->query_vars['suppress_filters'] )
+ $query->set( 'post_type', array( 'post', 'page') );
+ return $query;
+ }
